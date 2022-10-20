49f2e14e9034536415d35698bd5c24a0d476713e
tfnyyy23 <fadillahtifany@gmail.com>
2022-10-20 15:37:10 +0700

    migration

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\BookController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,4 +21,10 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::get('me', [AuthController::class, 'me']);
+
+Route::get('/books', [BookController::class, 'index']);
+Route::get('/book/{id}', [BookController::class, 'show']);
+Route::post('/books', [BookController::class, 'store']);
+Route::get('/books/{id}', [BookController::class, 'update']);
+Route::get('/books/{id}', [BookController::class, 'destroy']);
 
